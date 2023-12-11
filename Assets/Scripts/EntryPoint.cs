e66ed2a985eebaa5319155b51b211eb5d259bbcb
FoxsterDev <s.khalandachev@gmail.com>
2023-12-11 22:50:27 +0100

    Added git lfs config

--- Assets/Scripts/EntryPoint.cs
+++ Assets/Scripts/EntryPoint.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EntryPoint : MonoBehaviour
@@ -13,6 +11,7 @@ public class EntryPoint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
+
+   
 }
